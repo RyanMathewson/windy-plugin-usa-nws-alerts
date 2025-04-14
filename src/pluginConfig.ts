6aa3d3436f78f80235c1e2f9ebc5daa9cd3b3654
Ryan Mathewson <RyanMathewson@users.noreply.github.com>
2025-04-13 21:11:28 -0400

    Version bump

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-usa-nws-alerts',
-    version: '0.0.4',
+    version: '0.0.5',
     title: 'USA NWS Alerts',
     icon: '⚠️',
     description: 'Displays alerts from the USA National Weather Service.',
